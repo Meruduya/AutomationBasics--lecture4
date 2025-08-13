6499b63573a3d2dfaaaf137d625eb1d5953459b9
Veronika <korynaitroyd@gmail.com>
2025-08-13 15:13:47 +0300

    README.md

--- src/test/java/ru/netology/Tests.java
+++ src/test/java/ru/netology/Tests.java
@@ -21,20 +21,19 @@ class Tests {
     @Test
     void shouldSubmitFormSuccessfully() {
         String planningDate = generateDate(3, "dd.MM.yyyy");
-        String manualDate = "16.08.2025";
 
         Selenide.open("http://localhost:9999");
         SelenideElement form = $$("form").find(Condition.visible);
 
         form.$("[data-test-id=city] input").setValue("Москва");
-        form.$("[data-test-id=date] input").press(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE)
-                .setValue(manualDate);
+        form.$("[data-test-id=date] input").press(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        form.$("[data-test-id=date] input").setValue(planningDate);
         form.$("[data-test-id=name] input").setValue("Иванов Иван");
         form.$("[data-test-id=phone] input").setValue("+79272000000");
         form.$("[data-test-id=agreement]").click();
         form.$("[class='button__text']").click();
         $("[data-test-id='notification']")
-                .shouldHave(Condition.text("Встреча успешно забронирована на " + manualDate), Duration.ofSeconds(15));
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15));
     }
 }
 
